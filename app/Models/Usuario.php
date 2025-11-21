1c8224366c8d74d2c04a6fc6c4e5376a5584e7a7
DuriChaska <kimduri2004reina@gmail.com>
2025-11-20 19:42:16 -0600

    Modelo y Migración de Usuarios

---
+++ app/Models/Usuario.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
+class Usuario extends Authenticatable 
+{
+   
+    public function rol(): BelongsTo
+    {
+        return $this->belongsTo(Rol::class);
+    }
+    
+    // Un Usuario realiza muchos Movimientos
+    public function movimientos(): HasMany
+    {
+        return $this->hasMany(Movimiento::class);
+    }
+
+}
