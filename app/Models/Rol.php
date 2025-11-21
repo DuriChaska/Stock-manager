b87ea77c4cbac6f7dc2e9d330353181002dda604
DuriChaska <kimduri2004reina@gmail.com>
2025-11-20 22:23:33 -0600

    Realizacion del login y del registro, e Imágenes añadidas

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Rol extends Model
 {
-    //
+    public function usuarios(): HasMany
+    {
+        return $this->hasMany(Usuario::class);
+    }
 }
