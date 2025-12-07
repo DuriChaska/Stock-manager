2404026dbfefda8899361a8b9be762cb3adca55e
CathyBV09 <buenrostrovalencia@gmail.com>
2025-12-06 21:12:51 -0600

    feat: Implementar vista de Registro de Producto (Formulario)

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,11 +1,20 @@
-*<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title', 'Gestión de Productos')
+
+@section('content')
+    <div class="container">
+        <h1>Listado de Productos</h1>
+        
+        {{-- Aquí iría un botón para crear un nuevo producto --}}
+        <a href="{{ route('productos.create') }}" class="btn btn-primary">Crear Nuevo Producto</a>
+        
+        {{-- Aquí se mostraría la tabla con los productos --}}
+        <p>Tabla de productos...</p>
+        
+        {{-- En un paso posterior, iterarías sobre los productos: --}}
+        {{-- @foreach ($productos as $producto) --}}
+            {{-- ... mostrar datos del producto ... --}}
+        {{-- @endforeach --}}
+    </div>
+@endsection
