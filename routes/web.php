93a5d341324f9bab1b7c05479cb246b0e2e19661
CathyBV09 <buenrostrovalencia@gmail.com>
2025-12-06 22:09:40 -0600

    Actualiazacion

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/inventario', [ProductoController::class, 'index'])->name('productos.index'); // Muestra la tabla
+    Route::get('/inventario/crear', [ProductoController::class, 'create'])->name('productos.create'); // Muestra el formulario
+    Route::post('/inventario', [ProductoController::class, 'store'])->name('productos.store'); // Procesa el formulario (Guardar)
 });
 
 require __DIR__.'/auth.php';
